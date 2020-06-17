c6a3151a3508dbe22fcb213a96ffff499f57f411
RIchmond Eribo <uyiosaeribo344@gmail.com>
2020-06-17 21:20:00 +0100

    last commit

---
+++ chapter 1/sanbo.js
@@ -0,0 +1,11 @@
+
+// strings
+// all i know in strings right now.
+
+let name = 'daniel'; //daniel here is a string
+let age = 45; // 45 is a number not  a string.
+
+// I can concatenate this 
+
+let result = `the boys name is ${name} and he is ${age} years old`; // this is template string
+console.log(result)
