45bea5f53c544a93dbda3931980f3f5a36fa1572
RIchmond Eribo <uyiosaeribo344@gmail.com>
2020-06-21 10:50:08 +0100

    variables understanding comma in a console log bracket

--- chapter 1/sanbo.js
+++ chapter 1/sanbo.js
@@ -1,11 +1,6 @@
+// variables & block
+let age = 30;
 
-// strings
-// all i know in strings right now.
-
-let name = 'daniel'; //daniel here is a string
-let age = 45; // 45 is a number not  a string.
-
-// I can concatenate this 
-
-let result = `the boys name is ${name} and he is ${age} years old`; // this is template string
-console.log(result)
+if (true){
+    console.log('outside the code block: ', age);
+}
