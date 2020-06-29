916c077f63ec438d3d3ad2db9cd74f6e5451119f
RIchmond Eribo <uyiosaeribo344@gmail.com>
2020-06-29 22:53:08 +0100

    comments on score

--- richmond quiz/app.js
+++ richmond quiz/app.js
@@ -2,6 +2,8 @@ const correctAnswers = ['A', 'B', 'A', 'A', 'A'];
 
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
+const Insult = document.querySelector('.insult');
+const myBlessing = document.querySelector('.myBlessing');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -18,12 +20,17 @@ form.addEventListener('submit', e => {
 
     //display Result
     // console.log(score);
-
-    
     result.classList.remove('d-none');
     scrollTo(0,0);
+
+    if (score < 50) {
+        Insult.classList.remove('d-none');
+    } else {
+        myBlessing.classList.remove('d-none');};
+
     
 
+    //animation of the score.
     let output = 0;
     const timer = setInterval( () => {
         result.querySelector('span').textContent= `${output}%`;
@@ -33,6 +40,9 @@ form.addEventListener('submit', e => {
             output++;
         }
     } ,10);
+
+    
+
 
 });
 
